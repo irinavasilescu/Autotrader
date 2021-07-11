d0a515d98ff60f734b0f164e5e1f23d166befd6a
Irina Alexandra Vasilescu | eMAG, Plat&Tech - Marketplace <alexandra.vasilescu@emag.ro>
2021-07-11 23:27:08 +0300

    added route for indexAction controller

--- src/CarBundle/Controller/DefaultController.php
+++ src/CarBundle/Controller/DefaultController.php
@@ -8,7 +8,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/our-cars")
      */
     public function indexAction()
     {
