9fa1e2fb66fd02aedc22bcf0d6d6098c88c65122
Irina Alexandra Vasilescu | eMAG, Plat&Tech - Marketplace <alexandra.vasilescu@emag.ro>
2021-11-20 19:36:14 +0200

    displayed some cars in the template

--- src/AppBundle/Menu/Builder.php
+++ src/AppBundle/Menu/Builder.php
@@ -9,7 +9,7 @@ class Builder
     public function mainMenu(MenuFactory $factory, array $options)
     {
         $menu = $factory->createItem('root');
-        $menu->setChildrenAttribute('class', 'nav navbar-nav');
+        $menu->setChildrenAttribute('class', 'navbar-nav me-auto mb-2 mb-lg-0');
         $menu->addChild('Home', ['route' => 'homepage']);
         $menu->addChild('Offer', ['route' => 'offer']);
         return $menu;
