f5328b481cfe9b58eadd6da1d98b3e0a9c0faf4c
Irina Alexandra Vasilescu | eMAG, Plat&Tech - Marketplace <alexandra.vasilescu@emag.ro>
2021-11-21 15:55:53 +0200

    added new page /car/{id} to view more specific info

--- src/CarBundle/Controller/DefaultController.php
+++ src/CarBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace CarBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -15,5 +16,17 @@ class DefaultController extends Controller
         $carRepository = $this->getDoctrine()->getRepository('CarBundle:Car');
         $cars = $carRepository->findAll();
         return $this->render('@Car/Default/index.html.twig', ['cars' => $cars]);
+    }
+
+    /**
+     * @param $id
+     * @Route("/car/{id}", name="show_car")
+     * @return Response
+     */
+    public function showAction($id)
+    {
+        $carRepository = $this->getDoctrine()->getRepository('CarBundle:Car');
+        $car = $carRepository->find($id);
+        return $this->render('@Car/Default/show.html.twig', ['car' => $car]);
     }
 }
